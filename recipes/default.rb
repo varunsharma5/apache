c4fb6a8f9f4477ab87e54966cfa68ca383fc392f
Varun Sharma <varunge@gmail.com>
2021-06-07 20:32:23 +0000

    initial version

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,5 +1,14 @@
-#
-# Cookbook:: apache
-# Recipe:: default
-#
-# Copyright:: 2021, The Authors, All Rights Reserved.
+apt_update
+package 'net-tools' do 
+  action :install
+end
+package 'apache2' do 
+  action :install
+end
+file '/var/www/html/index.html' do 
+   action :create
+   content 'Hello Pipeline World!'
+end
+service 'apache2' do 
+  action [ :enable, :start ]
+end
