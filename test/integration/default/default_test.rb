44dc9b51d355ca41fec2eada7abcf2e45488c84f
Varun Sharma <varunge@gmail.com>
2021-06-08 15:29:49 +0000

    Added inspec test file

--- test/integration/default/default_test.rb
+++ test/integration/default/default_test.rb
@@ -2,15 +2,20 @@
 
 # The Chef InSpec reference, with examples and extensive documentation, can be
 # found at https://docs.chef.io/inspec/resources/
-
-unless os.windows?
-  # This is an example test, replace with your own test.
-  describe user('root'), :skip do
-    it { should exist }
-  end
+describe port(80) do
+  ir { should be_listening }
 end
 
-# This is an example test, replace it with your own test.
-describe port(80), :skip do
-  it { should_not be_listening }
+describe package('apache2') do
+  it { should be_installed }
+end
+
+describe file ('/var/www/html/index.html') do
+  it { should exist }
+  its('content') { should match (/Hello Pipeline World!/) }
+end
+
+describe upstart_service('apache2') do
+  it { should be_enabled}
+  it { should be_running}
 end
